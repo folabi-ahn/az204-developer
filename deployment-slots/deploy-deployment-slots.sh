1856f7b8e1762d8044a9bb6f124dca3abcddb594
AHOUNOU Folabi <folabi.ahounou@hatec.tech>
2024-02-17 09:29:25 +0100

    wip : deploy changes to staging slot
    Can't deploy changes to staging slot to process the slot swap

--- deployment-slots/deploy-deployment-slots.sh
+++ deployment-slots/deploy-deployment-slots.sh
@@ -10,13 +10,61 @@
 let "randomIdentifier=$RANDOM*$RANDOM"
 location="westeurope"
 resourceGroup="deployment-slots-rg-$randomIdentifier"
+appServicePlan="deployment-slots-app-service-plan-$randomIdentifier"
+webapp="deployment-slots-webapp-$randomIdentifier"
+webappName="my-webapp"
+webappPublish="my-webapp-publish"
+dotnetVersion="dotnet:6" # Change this to your default target framework version 
+
+# Create new .net webapp
+echo "Creating $webappName"
+dotnet new webapp -o my-webapp --force
+
+# Moving to webapp directory
+echo "Moving to webapp directory"
+cd $webappName/
 
 # Create resource group
 echo "Creating resource group $resourceGroup in $location"
 az group create -l $location -n $resourceGroup
 
+# Create a webapp and deploy code from a local workspace to the app
+az webapp up -g $resourceGroup -p $appServicePlan -n $webapp -l $location -r $dotnetVersion --sku S1
+
+# Website in production slot
+site="http://$webapp.azurewebsites.net"
+echo $site
+curl "$site"
+
+# Creating staging slot
+echo "Creating staging slot"
+az webapp deployment slot create -g $resourceGroup -n $webapp -s staging
+
+# Update Index.html content file
+echo "Updating Index.html file"
+sed -i -e 's/Welcome/Welcome in staging slot/g' Pages/Index.cshtml
+
+# Dotnet publish
+echo "Dotnet publish new changes"
+dotnet publish -o $webappPublish
+
+# Create zip
+echo "Creating zip"
+zip $webappPublish.zip $webappPublish/
+
+# Upload changes to staging slot
+echo "Upload changes to staging slot"
+az webapp deployment source config-zip -g $resourceGroup -n $webapp --src $webappPublish.zip -s staging
+
+# Swap slots
+# echo "Swaping slot production & staging"
+# az webapp deployment slot swap  -g $resourceGroup -n $webapp --slot staging \
+#     --target-slot production
 
 # </FullScript>
 
+# echo "Deleting $webappName"
+#rm -rf $webappName/
+
 # echo "Deleting all resources"
-# az group delete --name $resourceGroup -y
+# az group delete -n $resourceGroup -y
