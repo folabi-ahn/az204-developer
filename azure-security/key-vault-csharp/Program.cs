3d05509b586da4068c00e4a5cd54962f3c95ec87
AHOUNOU Folabi <folabi.ahounou@hatec.tech>
2024-03-05 20:24:16 +0100

    azure security: how to key vault csharp
    - create secret
    - read secret

---
+++ azure-security/key-vault-csharp/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+string kvUri = "https://kv-folabi-ahn.vault.azure.net/";
+
+// authenticate and create client
+// make sure that you have access to manage secrets in the vault access policies
+var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+
+// add secret
+var secretName = "password";
+var secretValue = "p@ssword1234";
+await client.SetSecretAsync(secretName, secretValue);
+
+// read secret
+var secret = await client.GetSecretAsync(secretName);
+Console.WriteLine(secret.Value.Value);
